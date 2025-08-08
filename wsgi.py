e8fbde03b95867c72b58b3b06ff208d876acfbc7
Alkhidmat Digital <alkhidmat.digital@gmail.com>
2025-08-08 14:00:27 +0500

    removed wsgi.py

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-# Add your app directory to Python path
-sys.path.insert(0, os.path.dirname(__file__))
-
-# Import the Flask application from app.py
-from app import application
