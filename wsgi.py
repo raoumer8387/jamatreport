6631781cae183e004468796904d0a2cced6184ec
Alkhidmat Digital <alkhidmat.digital@gmail.com>
2025-08-08 13:54:50 +0500

    changes in passenger wspi

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+# Add the app directory to the Python path
 sys.path.insert(0, os.path.dirname(__file__))
 
-from app1 import app as application 
+# Import the Flask app object from this file
+from main import application
