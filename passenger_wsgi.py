52276e5516a2fc4692f78836fa5c2534c3e3b2ff
Alkhidmat Digital <alkhidmat.digital@gmail.com>
2025-08-08 13:52:36 +0500

    Updated passender wspi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,7 +5,7 @@ import sys
 sys.path.insert(0, os.path.dirname(__file__))
 
 # Import the Flask app
-from app1 import app as application
+from app import app as application
 
 # For debugging (remove in production)
 if __name__ == '__main__':
